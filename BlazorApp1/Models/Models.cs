f3429de06327ed0ccb3ec3e10f347d973680496e
Arvinder <arvc@chevron.com>
2020-06-05 12:56:56 -0500

    initial checkin

---
+++ BlazorApp1/Models/Models.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlazorApp1.Models
+{
+    public class CustomerDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Attr1 { get; set; }
+        public string Attr2 { get; set; }
+        public string Attr3 { get; set; }
+        public string Attr4 { get; set; }
+        public string Attr5 { get; set; }
+        public DateTime? Attr6 { get; set; }
+        public DateTime? Attr7 { get; set; }
+        public List<OrderDto> Orders { get; set; }
+
+    }
+
+    public class OrderDto
+    {
+        public Guid Id { get; set; }
+        public int RowNumber { get; set; }
+        public string ProductName { get; set; }
+        public string Attr1 { get; set; }
+        public string Attr2 { get; set; }
+        public string Attr3 { get; set; }
+        public string Attr4 { get; set; }
+        public string Attr5 { get; set; }
+        public DateTime? Attr6 { get; set; }
+        public DateTime? Attr7 { get; set; }
+    }
+
+    public class DummyDto
+    {
+        public string Attr1 { get; set; }
+        public string Attr2 { get; set; }
+        public string Attr3 { get; set; }
+        public string Attr4 { get; set; }
+        public string Attr5 { get; set; }
+        public DateTime? Attr6 { get; set; }
+    }
+}
---
+++ BlazorApp1/Program.cs
@@ -0,0 +1,24 @@
+using Blazor.DragDrop.Core;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BlazorApp1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddBlazorDragDrop();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
